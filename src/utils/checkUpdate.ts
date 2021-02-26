a3fcbdbec91facd294507a01a8e17cc6db988960
sinlov <sinlovgmppt@gmail.com>
2021-02-26 12:56:12 +0800

    update checkUpdate.ts

--- src/utils/checkUpdate.ts
+++ src/utils/checkUpdate.ts
@@ -1,5 +1,6 @@
 import updateNotifier from 'update-notifier'
 import packageJsom from '../../package.json'
+import chalk from 'chalk'
 
 /**
  * check cli update
@@ -10,12 +11,13 @@ export const checkUpdate = (): void => {
       name: packageJsom.name,
       version: packageJsom.version
     },
+    updateCheckInterval:0 // close check interval
     // updateCheckInterval: 1000 * 60 * 60 * 24 // 1 day
-    updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
+    // updateCheckInterval: 1000 * 60 * 60 * 24 * 7 // 1 week
   })
   // notifier.notify({ message: 'Run `{updateCommand}` to update.' })
   notifier.notify({ message: 'Run `{updateCommand} -g` to update.' })
-  // if (notifier.update) {
-  //   console.log(notifier.update)
-  // }
+  if (notifier.update) {
+    console.log(chalk.yellow(`Latest version available: ${notifier.update.latest} , please update`))
+  }
 }
