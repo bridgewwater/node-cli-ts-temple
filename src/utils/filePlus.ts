6adc03d2de78d17dd5f6a78ef28a34d3ae69c57c
sinlov <sinlovgmppt@gmail.com>
2021-03-01 15:08:42 +0800

    add filePlus.ts recursivelyDeleteEmptyDirectories

--- src/utils/filePlus.ts
+++ src/utils/filePlus.ts
@@ -1,4 +1,6 @@
 import fs from 'fs'
+import path from 'path'
+import fsExtra from 'fs-extra'
 
 /**
  * check the path exists
@@ -7,10 +9,36 @@ import fs from 'fs'
 export const isExistPathSync = (p: string): boolean => fs.existsSync(p)
 
 /**
+ * check path is Directory
+ * @param p - path
+ */
+export const isPathDirectorySync = (p: string): boolean => fs.lstatSync(p).isDirectory()
+
+/**
  * check dir is Empty
  * @param p
  */
 export const isDirEmptySync = (p: string): boolean => fs.readdirSync(p).length === 0
+
+export const recursivelyDeleteEmptyDirectories = (rootPath: string, targetPath: string): Error | null => {
+  if (!fsExtra.existsSync(rootPath)) {
+    return new Error(`recursively delete empty directories root not exists: ${rootPath}`)
+  }
+  if (!fsExtra.existsSync(targetPath)) {
+    return new Error(`recursively delete empty directories target not exists: ${targetPath}`)
+  }
+  if (targetPath.indexOf(rootPath) === -1) {
+    return new Error(`recursively delete empty directories targetPath not contains rootPath: ${targetPath}`)
+  }
+  if (isDirEmptySync(targetPath)) {
+    fsExtra.removeSync(targetPath)
+  }
+  const subPath = path.dirname(targetPath)
+  if (subPath !== rootPath) {
+    return recursivelyDeleteEmptyDirectories(rootPath, subPath)
+  }
+  return null
+}
 
 /**
  * Fetch user home directory
