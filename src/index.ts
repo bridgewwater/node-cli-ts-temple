a5eb3983d519e40a6d193bbf3a291bd65f8ba184
sinlov <sinlovgmppt@gmail.com>
2021-03-02 23:58:12 +0800

    add error warning

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import { checkNodeVersion } from './utils/envCheck'
 import { initCommand } from './node-cli-ts-temple'
+import { ErrorAndExit } from './globalBiz'
+import { userConfigJsonPath } from './config/userConfig'
+import { binName } from './utils/pkgInfo'
 
 checkNodeVersion()
-initCommand()
+try {
+  initCommand()
+} catch (e) {
+  console.log(e)
+  ErrorAndExit(-127, `maybe config error, please remove config at: ${userConfigJsonPath()}
+or use: npm install -g ${binName()} to fix`)
+}
